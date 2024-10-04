0254604d676fcfe79d57dbc6f0c0e9a8ea2e312e
junior.silva <junior.silva@ibratan.com.br>
2024-10-04 12:19:40 -0300

    Arquivo criado pela Ana

---
+++ trabalho_capitulo7_fase2.R
@@ -0,0 +1,67 @@
+#AnaBeatrizDuarteDomingues_RM510302_fase2_cap7
+#JuniorRodriguesDaSilva_RM559451_fase2_cap7
+
+install.packages("ggplot2")
+library(ggplot2)
+
+#Dados da variável quantitativa
+volume_colhido <- c(8.5, 9, 8.7, 10.1, 9.5, 8.3, 8.1, 9.8, 9.2, 8.9, 8.6, 8.8, 8.4, 9.4, 8.7, 8.9, 9.7, 8.2, 9.1, 9.6, 8.7, 8.5, 10.2, 10.3, 9, 8.7, 9.9, 8.8, 9.1, 9.3)
+codigos <- c("MX001", "BTG02", "MX003", "MX004", "BTG05", "MX006", "BTG07", "MX008", "MX009", "BTG10", "MX011", "BTG12", "MX013", "MX014", "BTG15", "MX016", "BTG17", "MX018", "MX019", "BTG20", "MX021", "BTG22", "MX023", "MX024", "BTG25", "MX026", "BTG27", "MX028", "MX029", "BTG30")
+
+
+# 1. Medidas de Tendência Central
+media_volume <- mean(volume_colhido)
+mediana_volume <- median(volume_colhido)
+moda_volume <- as.numeric(names(sort(table(volume_colhido), decreasing = TRUE)[1]))
+
+# Exibindo as medidas de tendência central
+cat("Média:", media_volume, "\n") 
+cat("Mediana:", mediana_volume, "\n")
+cat("Moda:", moda_volume, "\n")
+
+# 2. Medidas de Dispersão
+maximo_volume <- max(volume_colhido)
+minimo_volume <- min(volume_colhido)
+variancia_volume <- var(volume_colhido)
+desvio_padrao_volume <- sd(volume_colhido)
+amplitude_volume <- max(volume_colhido) - min(volume_colhido)
+cv_volume <- (sd(volume_colhido)/mean(volume_colhido))*100
+
+# Exibindo as medidas de dispersão
+cat("Máximo:", maximo_volume, "\n")
+cat("Mínimo:", minimo_volume, "\n")
+cat("Variância:", variancia_volume, "\n")
+cat("Desvio Padrão:", desvio_padrao_volume, "\n")
+cat("Amplitude:", amplitude_volume, "\n")
+cat("Coeficiente de Variação:", cv_volume, "\n")
+
+
+
+
+# 3. Medidas Separatrizes
+quartis <- quantile(volume_colhido, probs =c(0.25, 0.50, 0.75))
+decis <- quantile(volume_colhido, probs = seq(0.1, 0.9, by = 0.1))
+centis <- quantile(volume_colhido, probs = seq(0.01, 0.99, by = 0.01))
+
+#Exibindo as medidas separatrizes
+cat("Quartis:", quartis, "\n")
+cat("Decis:", decis, "\n")
+cat("Centis:", centis, "\n")
+
+
+# 4. Análise Gráfica
+#Criando um data frame
+dados_volume <- data.frame(Código_Semente = codigos, Volume_Colhido = volume_colhido)
+#Criando um Gráfico de Barras
+ggplot(data = dados_volume, aes(x = codigos, y = volume_colhido)) + geom_bar(stat = "identity", fill = "pink") + labs(title = "Volume de Milho Colhido", x = "Códigos das Sementes",y = "Volume (Toneladas)")
+
+#Dados da Variável Qualitativa
+qualid_graos <- c("Bom", "Excelente", "Médio", "Bom", "Ruim", "Bom", "Médio", "Excelente", "Bom", "Médio", "Bom", "Excelente", "Médio", "Bom", "Ruim", "Bom", "Excelente", "Médio", "Bom", "Ruim", "Bom", "Médio", "Excelente", "Bom", "Médio", "Bom", "Excelente", "Ruim", "Bom", "Médio")
+
+#Análise Gráfica Variável Qualitativa
+#Criando um data frame
+dados_qualid <- data.frame(Sementes_Avaliadas = 1:30, Qualidade_Grãos = qualid_graos)
+
+ggplot(data = dados_qualid, aes(x = Sementes_Avaliadas, y = qualid_graos)) + geom_bar(stat = "identity", fill = "blue") + labs(title = "Qualidade dos Grãos Colhidos", x = "Quantidade de Sementes Avaliadas",y = "Qualidade dos Grãos")
+
+
